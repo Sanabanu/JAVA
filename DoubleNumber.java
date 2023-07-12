3f0aeabf717c2d50e02e974f136f56714aa80355
Sanabanu <86183120+Sanabanu@users.noreply.github.com>
2023-07-12 13:35:48 +0530

    Add files via upload

---
+++ DoubleNumber.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class DoubleNumber {
+	public static void main(String[] args) {
+		
+		Scanner scan=new Scanner(System.in);
+		int num=scan.nextInt();
+		int res=doubleTheNumber(num);
+		System.out.println(res);
+		
+		
+		
+	}
+	
+	public static int doubleTheNumber(int num)
+	{
+		return 2*num;
+	}
+
+}
